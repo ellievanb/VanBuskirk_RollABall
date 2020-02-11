b3384a042ee2680ddb8ca455392f870e09b81cba
ellievanb <54558569+ellievanb@users.noreply.github.com>
2020-02-11 13:17:04 -0500

    finish video 6

--- New Unity Project/Assets/_Scripts/PlayerController.cs
+++ New Unity Project/Assets/_Scripts/PlayerController.cs
@@ -6,12 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
 
-    Rigidbody rb;
+    private Rigidbody rb;
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
     }
 
     // Update is called once per frame
@@ -28,5 +30,14 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
     }
 }
