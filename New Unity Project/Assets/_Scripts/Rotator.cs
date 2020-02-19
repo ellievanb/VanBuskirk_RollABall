3138a589deccdca4f6c0c0f3c34e3d3403549e8a
ellievanb <54558569+ellievanb@users.noreply.github.com>
2020-02-18 21:07:50 -0500

    adds waypoints and movement to a few pick ups

--- New Unity Project/Assets/_Scripts/Rotator.cs
+++ New Unity Project/Assets/_Scripts/Rotator.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    
+
+    public GameObject[] waypoints;
+    int current = 0; //states which waypoint the object is moving towards
+    public float speed; // determines speed of gameobject
+    float WPradius = 1; //prevents gameobject from missing the waypoint
+
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime); // rotates yellow blocks
+   
+        // checks distance between waypoint and position of the gameobject
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++; // adds 1
+            if (current >= waypoints.Length) // checks if int value of current is larger than value of gameobject
+            {
+                current = 0; //returns to start, loops
+            }
+        }
+        // moves object to waypoint's position in the world
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
     }
 }
