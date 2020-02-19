8c7a765351ee0b1c14bcc26e71a861b04687ec83
ellievanb <54558569+ellievanb@users.noreply.github.com>
2020-02-18 20:43:39 -0500

    adds functioning waypoints for moving obstacles

---
+++ New Unity Project/Assets/_Scripts/BackAndForth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0; //states which waypoint the object is moving towards
+    public float speed; // determines speed of gameobject
+    float WPradius = 1; //prevents gameobject from missing the waypoint
+
+    // Update is called once per frame
+    void Update()
+    {
+        // checks distance between waypoint and position of the gameobject
+        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++; // adds 1
+            if (current >= waypoints.Length) // checks if int value of current is larger than value of gameobject
+            {
+                current = 0; //returns to start, loops
+            }
+        }
+        // moves object to waypoint's position in the world
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
